299622e259f722995caa8668d137d7434e324090
Ryan <44453329+rhruocco@users.noreply.github.com>
2020-10-30 21:28:57 -0500

    Add files via upload

--- battleship.cpp
+++ battleship.cpp
@@ -8,17 +8,23 @@ int * create_2d_array(const int rows, const int cols)
     return (int*)malloc(rows * cols *sizeof(int));
 }
 
-//Going east means increasing columns (j)
+//the check(direction) methods all have the same template
+//they check to see if a ship can be spawned facing that direction
 bool checkEast(int * board, const int rows, const int cols, int shipSize, int currentRow, int currentCol)
 {
+    //this int is the end of the ship, the offset at the end is cause the loops below start at the currentposition on the board
   int lastColShipTouches = currentCol + shipSize - 1;
+
   if (lastColShipTouches > cols)
   {
+      //checks to see if the last ship would be outside the board, returns false
       return false;
   }
 
+//this is the big loop that checks the current spot on the board's surroundings
    for (int j = currentCol; j <= lastColShipTouches ; j++)
    {
+       //this only runs if it's the first part of the ship. it checks 3 in the opposite direction
        if (j == currentCol && currentCol > 0)
        {
           for (int left = j - 1; left >= (j - 3); left--)
@@ -30,6 +36,8 @@ bool checkEast(int * board, const int rows, const int cols, int shipSize, int cu
               }
           }
        }
+
+       //same as the if above, but instead it checks 3 ahead of the last part of the ship
        else if (j == lastColShipTouches && currentCol < cols)
        {
           for (int right = j + 1; right <= (j + 3); right++)
@@ -40,6 +48,8 @@ bool checkEast(int * board, const int rows, const int cols, int shipSize, int cu
               }
           }
        }
+
+       //all the other parts of the ship use this check, to check (in this case) up and down
        else
        {
            for (int down = currentRow + 1; down <= (currentRow+ 3); down++)
@@ -62,6 +72,7 @@ bool checkEast(int * board, const int rows, const int cols, int shipSize, int cu
 }
 
 
+//edit of checkEast
 bool checkWest(int * board, const int rows, const int cols, int shipSize, int currentRow, int currentCol)
 {
   int lastColShipTouches = currentCol - shipSize + 1;
@@ -114,6 +125,7 @@ bool checkWest(int * board, const int rows, const int cols, int shipSize, int cu
    }
 }
 
+//edit of checkEast
 bool checkSouth(int * board, const int rows, const int cols, int shipSize, int currentRow, int currentCol)
 {
     int lastRowShipTouches = currentRow + shipSize - 1;
@@ -148,7 +160,6 @@ bool checkSouth(int * board, const int rows, const int cols, int shipSize, int c
           }
        }
        
-//gcc battleship.cpp -lstdc++
        else
        {
            std::cout << "in else\n";
@@ -166,28 +177,25 @@ bool checkSouth(int * board, const int rows, const int cols, int shipSize, int c
                    return false;
                }
            }
-       }
-       std::cout << "gagsada1111333agaShips...\n";
-       
+       }    
    }
-   std::cout << "gagagllaShips...\n";
 }
 
-//GOING NORTH MEANS DECREASING ROWS {i}
+//edit of checkEast
 bool checkNorth(int * board, const int rows, const int cols, int shipSize, int currentRow, int currentCol)
 {
-    int lastRowShipTouches = currentRow - shipSize + 1;
+    int lastRowShipTouches = currentRow + shipSize - 1;
    
-     if (lastRowShipTouches < 0)
+     if (lastRowShipTouches > rows)
   {
       return false;
   }
 
-   for (int i = currentRow; i >= lastRowShipTouches ; i--)
+   for (int i = currentRow; i <= lastRowShipTouches ; i++)
    {
        if (i == currentRow && currentRow < rows)
        {
-          for (int down = i + 1; down <= (i + 3); down++)
+          for (int down = i - 1; down <= (i - 3); down--)
           {
               if (*(board + down * cols + currentCol) == 1)
               {
@@ -197,7 +205,7 @@ bool checkNorth(int * board, const int rows, const int cols, int shipSize, int c
        }
        else if (i == lastRowShipTouches && currentRow > 0)
        {
-          for (int up = i - 1; up >= (i - 3); up--)
+          for (int up = i + 1; up >= (i + 3); up++)
           {
               if (*(board + up * cols + currentCol) == 1)
               {
@@ -226,26 +234,29 @@ bool checkNorth(int * board, const int rows, const int cols, int shipSize, int c
    }
 }
 
+//this calls each direction's check to see which directions a ship can spawn in given its coordinates
 char * avaliableDirections(int * board, const int rows, const int cols, int shipSize, int currentRow, int currentCol)
 {
+    //a list of all the directions
     char directions [4] = {'n','s','e','w'};
     if (!checkNorth(board, rows, cols, shipSize, currentRow, currentCol))
     {
         directions[0] = 'x';
     }
-    // if (!checkSouth(board, rows, cols, shipSize, currentRow, currentCol))
-    // {
-    //     directions[1] = 'x';
-    // }
-    // if (!checkEast(board, rows, cols, shipSize, currentRow, currentCol))
-    // {
-    //     directions[2] = 'x';
-    // }
+    if (!checkSouth(board, rows, cols, shipSize, currentRow, currentCol))
+    {
+        directions[1] = 'x';
+    }
+    if (!checkEast(board, rows, cols, shipSize, currentRow, currentCol))
+    {
+        directions[2] = 'x';
+    }
     if (!checkWest(board, rows, cols, shipSize, currentRow, currentCol))
     {
         directions[3] = 'x';
     }
-    std::cout << "Adding Ships...\n";
+
+    //replacing a direction with an x means that it cant spawn a ship in that direction. it returns the modified directions list
     return directions;
 }
 
@@ -263,7 +274,8 @@ bool shipsLeft(int * ships)  //checks if any ships are left to spawn
     return shipsLeft;
 }
 
-bool canThisShipSpawn(char * drections)
+bool canThisShipSpawn(char * drections) //checks to see if a ship spawn. it does this by simply checking if all the values of the array passed
+//in are 'x''s. if so, returns false
 {
     bool anyDirections = false;
     for(int i = 0; i < 3; i++)
@@ -273,10 +285,10 @@ bool canThisShipSpawn(char * drections)
             anyDirections = true;
         }
     }
-
     return anyDirections;
 }
 
+//spawns a ship based on the passed in direction, by going through a loop and changing the 0's to 1's
 int * spawnShip(int * board, const int rows, const int cols, int currentRow, int currentCol, int shipToSpawn, char direction)
 {
     int stopHere = 0;
@@ -312,37 +324,51 @@ int * spawnShip(int * board, const int rows, const int cols, int currentRow, int
         *(board + currentRow * cols + j) = 1;
     }
   }
+  //returns modified board
   return board;
 }
 
+//big method for putting every ship on the board
 int * shipGen(int * array, const int rows, const int cols)
 {
     //The 5 ships are: Carrier (occupies 5 spaces), Battleship (4), Cruiser (3), Submarine (3), and Destroyer (2).
     int ships[5] = {5,4,3,3,2};
 
+    //this loop checks every spot on the board
     for(int i = 0; i < rows; i++)
     {
         for (int j  = 0; j < cols; j++)
         {
-            //random ship gen
+            //only continues based on rng and if there's any ships left
             if ((rand() % 100) > 50 && shipsLeft(ships))
             {
+                //this picks a random ship from the list. the while loop runs until it picks a non 0 value
                 int randoShipsIndex = rand() % 4;
                 while (ships[randoShipsIndex] == 0)
                 {
                     randoShipsIndex = rand() % 4;
                 }
-                
 
+                //creates a list that avaliableFunctions fills with all the directions the current ship can spawn in.
+                //The current ship is whereever the loop is currently at, based on the row and colum index
+                //Ships are spawned on one spot, then extended east, west, south, or north depending on their size
+                
                 char * avalDirections = avaliableDirections(array,rows,cols,ships[randoShipsIndex],i,j);
 
+                //calls the spawn check function to see if a ship has directions it can spawn that arent forbidden on this current spot,
+                //as checked by the avalDirections above
                 if (canThisShipSpawn(avalDirections))
                 {
+
+                    //similar randomization process to the randoShipIndex variable above, only for an index for the array of allowed directions
                     int randoSpawnDirectionIndex = rand() % 3;
                     while (avalDirections[randoSpawnDirectionIndex] == 'x')
                     {
                         randoSpawnDirectionIndex = rand() % 3;
                     }
+
+                    //calls the spawnShip function to begin the spawning process at wherever the loop is at. (represented by i for row and j for col)
+                    //then, that ship is given a value of 0 in the ships array so it doesnt get spawned again
 
                     array = spawnShip(array,rows, cols, i, j, ships[randoShipsIndex], avalDirections[randoSpawnDirectionIndex]);
                     ships[randoShipsIndex] = 0;
@@ -351,11 +377,13 @@ int * shipGen(int * array, const int rows, const int cols)
             
         }
     }
+    //returns finished board with ships
     return array;
 }
 
 void printBoard(int * array, const int rows,  const int cols)
 {
+    //iterates thru the board and prints each space
     for(int i = 0; i < rows; i++)
     {
         for(int j = 0; j < rows; j++)
@@ -369,6 +397,7 @@ void printBoard(int * array, const int rows,  const int cols)
 
 int * boardGen(int size)
 {
+    //calls the 2darray function, returns its result
     int * board = create_2d_array(size, size);
 
     return board;
@@ -376,12 +405,12 @@ int * boardGen(int size)
 
 int main()
 {
+    //the player is asked for a board size, then the board is generated, given ships, and printed.
     int boardSize = 0;
 
     std::cout << "How big do you want the board to be?\nEnter 1 number, boards will be square:";
     
     std::cin >> boardSize;
-
 
     int * board = boardGen(boardSize);
     board = shipGen(board,boardSize,boardSize);
