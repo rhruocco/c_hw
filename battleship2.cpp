03025736914c0692f3a2ad11428c045be3ac86ea
Ryan <44453329+rhruocco@users.noreply.github.com>
2020-11-04 21:51:11 -0600

    Began rewriting ship check

--- battleship2.cpp
+++ battleship2.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <cstdlib>
-#include <time.h>;
+#include <ctime>
 
 
 int * board;
@@ -21,67 +21,34 @@ bool checker(int shipSize, int currentRow, int currentCol, char direction)
     //this bool will be returned at the end. it'll remain true until something makes it false
     bool canSpawn = true;
 
-    //THE BELOW VARIABLES ARE PARAMETERS FOR THE FOR LOOPS
+    //These are calculated below
+    //These are parameters for two loops that check if there's a ship 3 spaces away
+    //from the ship it's currently checking thats aligned the same (vertically or horizontal)
+    int threeAwayFromFirstPiece = 0;
+    int threeAwayFromLastPiece = 0;
 
-    //These project where the coordinates of the last piece of the ship will be if it's allowed to spawn, calculated below
-    //based on ship size, the coordinates of the bottom piece of the ship, and the direction it'll face.
-    int lastRowShipTouches = 0;
-    int lastColShipTouches = 0;
-    
-
-
-    //These are variable for special checks that are only done at the first and last pieces of the ship
-    //It's only necessary for the front and back pieces of the ship to check 3 spaces in their aligntment (horizontal or vertical)
-    //(for example; a WEST facing ship would need to check 3 spaces EAST of the first piece of the ship, and 3 WEST of the last piece)
-    //The non-bordering pieces of the ship check 3 spaces horizontally (if the ship is vertical) or vertically (if ship is horizontal)
-    
-    //TO WHOEVER READS THIS, I REALLY HOPED I EXPLAINED THAT WELL.
-
-    int threeSpacesVerticalOfTheFirstPiece = 0;
-    int threeSpacesHorizontalOfTheFirstPiece = 0;
-
-    int threeSpacesVerticalOfTheLastPiece = 0;
-    int threeSpacesHorizontalOfTheLastPiece = 0;
     
     //this switch statement changes the values above based on which direction the ship will spawn in.
     switch(direction)
     {
         case 'n':
-        lastRowShipTouches = currentRow + shipSize - 1;
-        threeSpacesVerticalOfTheFirstPiece = currentRow + 3;
-        threeSpacesVerticalOfTheLastPiece = lastRowShipTouches + 3;
+
+        //for (int i = currentRow; i >= )
         break;
 
         case 's':
-        lastRowShipTouches =  currentRow + shipSize + 1;
-        threeSpacesVerticalOfTheFirstPiece = currentRow - 3;
-        threeSpacesVerticalOfTheLastPiece = lastRowShipTouches - 3;
         break;
 
         case 'e':
-        lastColShipTouches = currentCol  + shipSize -1;
-        threeSpacesHorizontalOfTheFirstPiece = currentCol - 3;
-        threeSpacesHorizontalOfTheLastPiece = lastColShipTouches + 3;
         break;
 
         case 'w':
-        lastColShipTouches = currentCol - shipSize + 1;
-        threeSpacesHorizontalOfTheFirstPiece = currentCol + 3;
-        threeSpacesHorizontalOfTheLastPiece = lastColShipTouches - 3;
         break;
 
         default:
         break;
     }
-
-    //if the end piece of the ship will be out of bounds, the ship cannot spawn
-    if (lastRowShipTouches > boardSize || lastRowShipTouches < 0 || lastColShipTouches > boardSize || lastColShipTouches < 0)
-    {
-        canSpawn = false;
-    }
     return canSpawn;
-
-    
 }
 
 //this calls each direction's check to see which directions a ship can spawn in given its coordinates
@@ -119,70 +86,45 @@ bool shipsLeft(int * ships)  //checks if any ships are left to spawn
 //spawns a ship based on the passed in direction, by going through a loop and changing the 0's to 1's
 void spawnShip(int currentRow, int currentCol, int shipToSpawn, char direction)
 {
-    int rowOfFinalPiece = 0;
-    int colOfFinalPiece = 0;
-
-    //these decide whether the loop will increment north or south, and east or south of the board
-    //whien spawning the ship. 
-    int northOrSouth = 0;
-    int eastOrWest = 0;
-
-    //These are ints will be conditions for stopping the ship spawn loop.
-    //Because I cant just use a simple greater/less than operator, this is how I'm doin it.
-    //The spawn horizontal and vertical spawn loops will stop when they ARE EQUAL to these ints
-    int stopRowLoopHere = 0;
-    int stopColLoopHere = 0;
-
+    //the row or col (depending on the direction the ship's spawning) of the last piece
+    int lastPiece = 0;
+    std::cout <<"\nspawnShip\n";
     switch (direction)
     {
         case 'n':
-            rowOfFinalPiece = currentRow - shipToSpawn + 1;
-            colOfFinalPiece = currentCol;
-
-            northOrSouth = -1;
-            eastOrWest = 1;
-
-            stopRowLoopHere = rowOfFinalPiece - 1;
-            stopColLoopHere = currentCol + 1;
+            lastPiece = currentRow - shipToSpawn + 1;
+            for (int i = currentRow; i >= lastPiece; i--)
+            {
+                *(board + i* boardSize + currentCol) = 1;
+            }
         break;
 
         case 's':
-            rowOfFinalPiece = currentRow + shipToSpawn - 1;
-            colOfFinalPiece = currentCol;
-
-            northOrSouth = 1;
-            eastOrWest = 1;
-
-            stopRowLoopHere = rowOfFinalPiece + 1;
-            stopColLoopHere = currentCol + 1;
+            lastPiece = currentRow + shipToSpawn - 1;
+            for (int i = currentRow; i <= lastPiece; i++)
+            {
+                *(board + i* boardSize + currentCol) = 1;
+            }
         break;
 
         case 'e':
-            northOrSouth = 1;
-            eastOrWest = 1;
-
-            stopRowLoopHere = rowOfFinalPiece + 1;
-            stopColLoopHere = colOfFinalPiece + 1;
+            lastPiece = currentCol + shipToSpawn - 1;
+            for (int j = currentCol; j <= lastPiece; j++)
+            {
+                *(board + currentRow * boardSize + j) = 1;
+            }
         break;
 
         case 'w':
-            northOrSouth = 1;
-            eastOrWest = -1;
-
-            stopRowLoopHere = rowOfFinalPiece + 1;
-            stopColLoopHere = colOfFinalPiece - 1;
+            lastPiece = currentCol - shipToSpawn + 1;
+            for (int j = currentCol; j >= lastPiece; j--)
+            {
+                *(board + currentRow * boardSize + j) = 1;
+            }
         break;
 
         default:
         break;
-    }
-
-      for(int i = currentRow; i != rowOfFinalPiece; i += northOrSouth)
-    {
-        for(int j = currentCol; j != colOfFinalPiece; j += eastOrWest)
-        {
-            *(board + i * boardSize + currentCol) = 1;
-        }
     }
   }
 
@@ -192,7 +134,7 @@ void shipGen()
     char * avalDirections = new char;
     //The 5 ships are: Carrier (occupies 5 spaces), Battleship (4), Cruiser (3), Submarine (3), and Destroyer (2).
     int ships[5] = {5,4,3,3,2};
-    int shipSpawnRng = 52;
+    int shipSpawnRng = 56;
     int randoSpawnDirectionIndex = 0;
 
     int randoShipsIndex = 0;
@@ -231,7 +173,7 @@ void shipGen()
                 //calls the spawnShip function to begin the spawning process at wherever the loop is at. (represented by i for row and j for col)
                 //then, that ship is given a value of 0 in the ships array so it doesnt get spawned again
 
-                    spawnShip(i, j, 1, avalDirections[randoSpawnDirectionIndex]);
+                    spawnShip(i, j, ships[randoShipsIndex], avalDirections[randoSpawnDirectionIndex]);
                     ships[randoShipsIndex] = 0;
             }
             
@@ -243,6 +185,7 @@ void shipGen()
 //modified from: https://gist.github.com/kstatz12/1675e6823134f0c4437734148a9dcce3
 void printBoard()
 {
+    std::cout <<"\n";
     //iterates thru the board and prints each space
     for(int i = 0; i < boardSize; i++)
     {
@@ -268,7 +211,8 @@ int main()
     std::cout << std::endl;
     
     board = create_2d_array();
-    
+    srand (time(NULL));
+
     shipGen();
     printBoard();
 
